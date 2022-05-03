d57c9d559ef7f25cf996cf4d071e69806b983dcb
Philipp66904 <62964107+Philipp66904@users.noreply.github.com>
2022-05-03 10:38:24 +0200

    added Echo Off

--- DestroyNetwork.bat
+++ DestroyNetwork.bat
@@ -1,3 +1,4 @@
+@Echo Off
 netsh interface ip set dns name="WLAN" static 127.0.0.1 BOTH
 netsh interface ip set dns name="Ethernet" static 127.0.0.1 BOTH
 netsh interface ip set address "WLAN" static 127.0.0.1 255.255.255.255 127.0.0.1 1
