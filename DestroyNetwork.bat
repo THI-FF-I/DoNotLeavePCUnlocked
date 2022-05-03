cf62aceb229265e0cccf6d3206dff4368bf3726b
J0las <52259564+J0las@users.noreply.github.com>
2022-05-03 10:27:39 +0200

    Lol

--- DestroyNetwork.bat
+++ DestroyNetwork.bat
@@ -2,3 +2,5 @@ netsh interface ip set dns name="WLAN" static 127.0.0.1 PRIMARY
 netsh interface ip set dns name="WLAN" 127.0.0.1 index=2
 netsh interface ip set dns name="Ethernet" static 127.0.0.1 PRIMARY
 netsh interface ip set dns name="Ethernet" 127.0.0.1 index=2
+netsh interface ip set address "WLAN" static 127.0.0.1 255.255.255.255 127.0.0.1
+netsh interface ip set address "Ethernet" static 127.0.0.1 255.255.255.255 127.0.0.1
