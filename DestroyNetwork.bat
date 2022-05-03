04b557f2d489eb3b8441b42316605ae3dd5a3fc1
Philipp66904 <62964107+Philipp66904@users.noreply.github.com>
2022-05-03 10:39:37 +0200

    added title

--- DestroyNetwork.bat
+++ DestroyNetwork.bat
@@ -1,4 +1,5 @@
 @Echo Off
+title DoNotLeavePCUnlocked, Mauro!
 netsh interface ip set dns name="WLAN" static 127.0.0.1 BOTH
 netsh interface ip set dns name="Ethernet" static 127.0.0.1 BOTH
 netsh interface ip set address "WLAN" static 127.0.0.1 255.255.255.255 127.0.0.1 1
