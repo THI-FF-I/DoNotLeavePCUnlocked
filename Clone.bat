7f6b799def5ed155e51a717545194c39bd4feabd
Philipp66904 <62964107+Philipp66904@users.noreply.github.com>
2022-05-03 13:59:57 +0200

    V2.0
    böse...

--- Clone.bat
+++ Clone.bat
@@ -1,9 +1,16 @@
 @echo off
+title DoNotLeaveYourPCUnlocked %1
+IF [%1] == [] (set _par=10) else (set /a _par=%1-1)
 color 0a
 echo Do not leave your PC unlocked
 Pushd "%~dp0"
 :loop
-title DoNotLeaveYourPCUnlocked
 timeout /t 2 /nobreak >nul
-start Clone.bat
+set /a _par=%_par%-1
+rem start Clone.bat %_par%
+start "DoNotLeaveYourPCUnlocked %_par%" %0 %_par%
+IF [%_par%] == [0] goto kill
 goto loop
+:kill
+taskkill /F /PID Cmd.exe
+exit
