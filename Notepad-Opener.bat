5ebc547f89d3f25dc3bfd73984146234d1b3f601
Philipp66904 <62964107+Philipp66904@users.noreply.github.com>
2022-05-04 13:43:06 +0200

    V2.0

--- Notepad-Opener.bat
+++ Notepad-Opener.bat
@@ -1,5 +1,6 @@
 @echo off
-for /l %%i in (1, 1, 50) DO start "" Notepad
+IF [%1] == [] (set _par=30) else (set /a _par=%1)
+for /l %%i in (1, 1, %_par%) DO start "" Notepad
 
 rem Reverse:
 rem taskkill /F /PID Notepad.exe
