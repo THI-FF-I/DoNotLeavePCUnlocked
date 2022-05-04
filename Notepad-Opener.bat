9d4c168284e9e0ad8d33090be0045865c9f246e7
Philipp66904 <62964107+Philipp66904@users.noreply.github.com>
2022-05-04 13:45:46 +0200

    V3.0
    enter 0 to kill all notepad windows

--- Notepad-Opener.bat
+++ Notepad-Opener.bat
@@ -1,6 +1,13 @@
 @echo off
-IF [%1] == [] (set _par=30) else (set /a _par=%1)
+IF [%1] == [] (set _par=10) else (set /a _par=%1)
+IF [%1] == [0] goto kill
 for /l %%i in (1, 1, %_par%) DO start "" Notepad
+goto end
+
+:end
+exit
 
 rem Reverse:
-rem taskkill /F /PID Notepad.exe
+:kill
+taskkill /F /PID Notepad.exe
+goto end
